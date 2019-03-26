ab1b4c69ccd78ced0800d895222af434632f900e
Christian Andolf <christian@weahead.se>
2019-03-26 17:35:45 +0100

    Created embryo of wrap-cli to start project and install dependencies. Still not resolving dependencies before installing.

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var chalk = require("chalk");
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split(".");
+var major = semver[0];
+
+if (major < 10) {
+  console.error(
+    chalk.red(
+      "You are running Node " +
+        currentNodeVersion +
+        ".\n" +
+        "Create React App requires Node 10 or higher. \n" +
+        "Please update your version of Node."
+    )
+  );
+  process.exit(1);
+}
+
+require("./wrap");
---
+++ wrap.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const program = require("commander");
+const chalk = require("chalk");
+
+const createApp = require("./lib/createApp");
+
+const packageJSON = require("./package.json");
+
+let projectName;
+
+program
+  .version(packageJSON.version)
+  .arguments("<project-directory>")
+  .usage(`${chalk.green("<project-directory>")} [options]`)
+  .action(name => {
+    projectName = name;
+  })
+  .option("--verbose", "print additional logs")
+  .parse(process.argv);
+
+if (projectName === undefined) {
+  console.error("Please specify the project directory:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("<project-directory>")}`
+  );
+  console.log();
+  console.log("For example:");
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green("my-react-app")}`);
+  process.exit(1);
+}
+
+createApp(projectName, program.verbose).then(() => {
+  console.log("all done!");
+});
