3fb35b1e84993dbff0124d2138a1760a87879915
Christian Andolf <christian@weahead.se>
2019-10-08 15:21:33 +0200

    fix: added config packages

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+npx install-peerdeps -d -x '-E' @weahead/husky-config
+npx install-peerdeps -d -x '-E' @weahead/markdownlint-config
+npx install-peerdeps -d -x '-E' @weahead/lint-staged-config
+npx install-peerdeps -d -x '-E' @weahead/prettier-config
+npx install-peerdeps -d -x '-E' @weahead/commitlint-config
+npx install-peerdeps -d -x '-E' @weahead/eslint-config-tool
